f80ad59563a89d1fd4efc54a7c945bce19a05281
William Steck <steckwilliam@gmail.com>
2020-07-21 18:40:19 -0500

    removed comment

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-// Heroku injects environmental vairable
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
