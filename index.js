57a5f51e6e4a063d3131828f7627766d3f5ce98e
William Steck <steckwilliam@gmail.com>
2020-07-22 16:47:08 -0500

    Successfully connected to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
 const app = express();
+//Heroku injects dynamic env port var
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
