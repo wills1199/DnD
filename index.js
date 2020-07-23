2aa88e2fff5d81e5072666cc43d41febe9d0a595
William Steck <steckwilliam@gmail.com>
2020-07-22 19:23:42 -0500

    passport/cookie//login/logout working

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
 const app = express();
-//Heroku injects dynamic env port var
-const PORT = process.env.PORT || 3000;
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 mongoose.connection.once('open', () => {
@@ -15,4 +24,6 @@ mongoose.connection.once('open', () => {
 
 require('./routes/authRoutes')(app);
 
+//Heroku injects dynamic env port var
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
